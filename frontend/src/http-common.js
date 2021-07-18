963e6a8c9feefdd21a7f29e1c714108cadb1fc36
Daniel Bellmas <76179660+danielbellmas@users.noreply.github.com>
2021-07-18 22:44:46 +0300

    make it better

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1/plants",
+  baseURL: "http://localhost:5000/api/v1/plants", //"https://grow-shop-backend.herokuapp.com/api/v1/plants"
   headers: {
     "Content-type": "application/json",
   },

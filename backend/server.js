2ff1f0eec84ddb9c702eb3811362bf5ff219e943
Daniel Bellmas <76179660+danielbellmas@users.noreply.github.com>
2021-07-19 11:27:25 +0300

    using the production build of React

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,27 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import plants from "./api/plants.route.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/plants", plants);
-app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+// app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api running");
+  });
+}
 
 export default app;

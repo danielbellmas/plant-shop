49255d1622777767f00161c744698ac0913add61
Daniel Bellmas <76179660+danielbellmas@users.noreply.github.com>
2021-11-06 19:13:50 +0200

    Add min-height to every plant card

--- frontend/src/styles/plant-style.js
+++ frontend/src/styles/plant-style.js
@@ -2,7 +2,8 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles(() => ({
   root: {
-    width: "85%",
+    minHeight: "31rem",
+    width: "80%",
     margin: "auto",
     boxShadow: "0.1rem 0.2rem 0.5rem",
   },

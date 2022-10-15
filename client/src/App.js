1b5c11260d4cf1c0acf63eb51f18844e9d9b5f0b
Daniel Bellmas <dbalmas7@gmail.com>
2022-10-15 18:18:17 +0300

    Preload fonts
    https://dev.to/danielbellmas/preload-fonts-in-react-ll4-temp-slug-5683904

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
 import ReactGA from "react-ga";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import "./App.css";
 const Cart = React.lazy(() => import("./components/Cart/Cart"));
 const Nav = React.lazy(() => import("./components/Nav"));
 const PlantList = React.lazy(() => import("./components/PlantList"));
@@ -18,45 +19,58 @@ function App() {
   ReactGA.pageview(window.location.pathname + window.location.search);
 
   return (
-    <Suspense
-      fallback={
-        <div
-          style={{
-            display: "grid",
-            placeItems: "center",
-            alignItems: "center",
-            height: "100vh",
-            background:
-              "linear-gradient(244.36deg,rgba(33, 107, 101, 0.1) 10.28%,rgba(33, 107, 101, 0.2) 109.64%",
-          }}
-        >
-          <img
-            src="/images/preloader.gif"
-            alt="loading..."
-            style={{ width: "100px", height: "auto" }}
-          />
-        </div>
-      }
-    >
-      <Router>
-        <Nav />
-        <div id="page-container">
-          <div id="content-wrap">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/products" component={PlantList} />
-              <Route path="/products/:id" component={PlantDetails} />
-              <Route exact path="/cart" component={Cart} />
-              <Route exact path="/checkout" component={Checkout} />
-              <Route exact path="/contacts" component={Contacts} />
-              <Route exact path="/signin" component={SignIn} />
-              <Route exact path="/signup" component={SignUp} />
-            </Switch>
+    <HelmetProvider>
+      <Helmet>
+        <title>Plant Shop</title>
+
+        <link
+          rel="preload"
+          href="https://fonts.gstatic.com/s/poppins/v20/pxiEyp8kv8JHgFVrJJfecnFHGPc.woff2"
+          as="font"
+          crossOrigin
+        />
+      </Helmet>
+
+      <Suspense
+        fallback={
+          <div
+            style={{
+              display: "grid",
+              placeItems: "center",
+              alignItems: "center",
+              height: "100vh",
+              background:
+                "linear-gradient(244.36deg,rgba(33, 107, 101, 0.1) 10.28%,rgba(33, 107, 101, 0.2) 109.64%",
+            }}
+          >
+            <img
+              src="/images/preloader.gif"
+              alt="loading..."
+              style={{ width: "100px", height: "auto" }}
+            />
           </div>
-          <Footer />
-        </div>
-      </Router>
-    </Suspense>
+        }
+      >
+        <Router>
+          <Nav />
+          <div id="page-container">
+            <div id="content-wrap">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/products" component={PlantList} />
+                <Route path="/products/:id" component={PlantDetails} />
+                <Route exact path="/cart" component={Cart} />
+                <Route exact path="/checkout" component={Checkout} />
+                <Route exact path="/contacts" component={Contacts} />
+                <Route exact path="/signin" component={SignIn} />
+                <Route exact path="/signup" component={SignUp} />
+              </Switch>
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </Suspense>
+    </HelmetProvider>
   );
 }
 
